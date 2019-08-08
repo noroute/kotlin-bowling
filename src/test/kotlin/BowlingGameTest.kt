0c307070f781723414db1b3c168cc9dc88f366ae
Florian Thiel <github@noroute.de>
2019-08-08 22:18:28 +0200

    Trivial green

--- src/test/kotlin/BowlingGameTest.kt
+++ src/test/kotlin/BowlingGameTest.kt
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test
 class BowlingGameTest{
 
     @Test
-    fun trivial(){
-        assertThat(BowlingGame("1").score()).isEqualTo(2)
+    fun incompleteGameNumbersOnly(){
+        assertThat(BowlingGame("1").score()).isEqualTo(1)
+        assertThat(BowlingGame("123").score()).isEqualTo(6)
     }
 }

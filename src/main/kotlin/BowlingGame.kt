984cf320de99bb9b36a8ec29b3bbf06aea08062e
Florian Thiel <github@noroute.de>
2019-08-10 00:12:32 +0200

    Added SPARE and STRIKE

--- src/main/kotlin/BowlingGame.kt
+++ src/main/kotlin/BowlingGame.kt
@@ -1,19 +1,57 @@
-import java.lang.IllegalStateException
+import kotlin.math.max
 
 data class BowlingGame(private val frameString: String) {
 
     private val FRAMES_PER_GAME = 10
+    private val SPARE = '/'
+    private val STRIKE = 'X'
 
-    private val rolls : String
-    private val isComplete : Boolean
+    private val rolls: String
+    private val isComplete: Boolean
 
     init {
         isComplete = frameString.split(",").size == FRAMES_PER_GAME
-        rolls = frameString.filter { c -> c != ','}
+        rolls = frameString.filter { c -> c != ',' }
     }
 
-    fun score() : Int {
-        return if (isComplete) rolls.map(Character::getNumericValue).sum() else
+    fun score(): Int {
+        return if (isComplete) rolls.mapIndexed(::scoreForIndex).sum() else
             throw IllegalStateException("Incomplete game")
+    }
+
+    private fun scoreForIndex(index: Int, char: Char): Int {
+        val value = valueForIndex(index, char) + bonusForIndex(index)
+        println("Value for $index:$char = $value")
+        return value
+    }
+
+    private fun valueForIndex(index: Int, char: Char): Int {
+        when {
+            char.isDigit() -> return Character.getNumericValue(char)
+            char == SPARE -> return 10 - valueForIndex(index - 1, rolls[index - 1])
+            char == STRIKE -> return 10
+        }
+        throw java.lang.IllegalStateException("Illegal roll found")
+    }
+
+    private fun bonusForIndex(index: Int): Int {
+        if (index == 0) {
+            return 0
+        }
+        if (previousRoll(index) == SPARE || previousRoll(index) == STRIKE) {
+            return valueForIndex(index, rolls[index])
+        } else if (twoRollsPrevious(index) == STRIKE && index > 1) {
+            return valueForIndex(index, rolls[index])
+        } else {
+            return 0
+        }
+    }
+
+    private fun previousRoll(currentIndex: Int): Char {
+        return rolls[max(0, currentIndex - 1)]
+    }
+
+    private fun twoRollsPrevious(currentIndex: Int): Char {
+        return rolls[max(0, currentIndex - 2)]
     }
 }
--- src/test/kotlin/BowlingGameTest.kt
+++ src/test/kotlin/BowlingGameTest.kt
@@ -15,4 +15,34 @@ class BowlingGameTest {
     fun completeGamesShouldBeScored() {
         assertThat(BowlingGame("10,00,00,00,00,00,00,00,00,00").score()).isEqualTo(1)
     }
+
+    @Test
+    fun scoresTrivialSpareExampleCorrectly() {
+        assertThat(BowlingGame("1/,11,00,00,00,00,00,00,00,00").score()).isEqualTo(13)
+    }
+
+    @Test
+    fun scoresTrivialSpareExampleInLastFrameCorrectly() {
+        assertThat(BowlingGame("00,00,00,00,00,00,00,00,00,1/1").score()).isEqualTo(12)
+    }
+
+    @Test
+    fun scoresTrivialStrikeExampleCorrectly() {
+        assertThat(BowlingGame("X,11,00,00,00,00,00,00,00,00").score()).isEqualTo(14)
+    }
+
+    @Test
+    fun scoresTrivialStrikeExampleInLastFrameCorrectly() {
+        assertThat(BowlingGame("00,00,00,00,00,00,00,00,00,X11").score()).isEqualTo(14)
+    }
+
+    @Test
+    fun scoresCombinedSpareAndStrikeExampleCorrectly() {
+        assertThat(BowlingGame("1/,X,11,00,00,00,00,00,00,00").score()).isEqualTo(34)
+    }
+
+    @Test
+    fun exampleGameShouldBeScoredCorrectly() {
+        assertThat(BowlingGame("14,45,6/,5/,X,01,7/,6/,X,2/6").score()).isEqualTo(139)
+    }
 }
